4386e9a71313c23ca843b7c3e0bd6deca11f541a
f.falcone <f.falcone@unibs.it>
2016-04-28 10:28:38 +0200

    group activity

--- AppWOW/app/src/main/java/it/unibs/appwow/NavigationActivity.java
+++ AppWOW/app/src/main/java/it/unibs/appwow/NavigationActivity.java
@@ -1,5 +1,6 @@
 package it.unibs.appwow;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -81,6 +82,8 @@ public class NavigationActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
+            final Intent intent = new Intent(this,GroupActivity.class);
+            startActivity(intent);
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
