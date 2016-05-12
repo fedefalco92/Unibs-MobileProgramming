9f174a8e9a1ef31227ec43392f065a0456bbf5ef
f.falcone <f.falcone@unibs.it>
2016-05-12 12:42:11 +0200

    LocalDB_DAO abstract

--- AppWOW/app/src/main/java/it/unibs/appwow/database/LocalDB_DAO.java
+++ AppWOW/app/src/main/java/it/unibs/appwow/database/LocalDB_DAO.java
@@ -1,13 +1,26 @@
 package it.unibs.appwow.database;
 
+import android.database.sqlite.SQLiteDatabase;
+
 import java.util.List;
+
+import it.unibs.appwow.MyApplication;
 
 /**
  * Created by Massi on 12/05/2016.
  */
-public interface LocalDB_DAO {
-    public void open();
-    public void close();
+public abstract class LocalDB_DAO {
+    private SQLiteDatabase database;
+    private AppSQLiteHelper dbHelper;
 
-    // TODO: 12/05/2016 CLASSE ASTRATTA 
+    public void open() {
+        if(dbHelper == null) {
+            dbHelper = new AppSQLiteHelper(MyApplication.getAppContext());
+        }
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
 }
