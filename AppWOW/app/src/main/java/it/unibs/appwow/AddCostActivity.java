2474937d621837011da2c77c8bde517bd1ac9330
Maff3x <ale725@hotmail.it>
2016-06-23 10:53:20 +0200

    Aggiunto pickplace

--- AppWOW/app/src/main/java/it/unibs/appwow/AddCostActivity.java
+++ AppWOW/app/src/main/java/it/unibs/appwow/AddCostActivity.java
@@ -1,21 +1,36 @@
 package it.unibs.appwow;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
 import it.unibs.appwow.fragments.CostsFragment;
 import it.unibs.appwow.models.parc.GroupModel;
 import it.unibs.appwow.models.parc.LocalUser;
+import it.unibs.appwow.utils.DecimalDigitsInputFilter;
 
 public class AddCostActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PLACE_PICKER = 1;
 
     private LocalUser mUser;
     private GroupModel mGroup;
@@ -25,6 +40,12 @@ public class AddCostActivity extends AppCompatActivity {
     private TextView mNotes;
     private Button mAddPosition;
     private Button mAddCost;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient mClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,43 +56,12 @@ public class AddCostActivity extends AppCompatActivity {
 
         mName = (TextView) findViewById(R.id.add_cost_name);
 
-        mAmount = (TextView)findViewById(R.id.add_cost_amount);
+        mAmount = (TextView) findViewById(R.id.add_cost_amount);
+        mAmount.setFilters(new InputFilter[]{new DecimalDigitsInputFilter(7, 2)});
 
         mNotes = (TextView) findViewById(R.id.add_cost_notes);
 
         mAddPosition = (Button) findViewById(R.id.add_cost_add_position_button);
-        mAddPosition.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        // TODO: 22/06/2016 IMPLEMENTARE AGGIUNTA POSIZIONE
-                        Toast.makeText(AddCostActivity.this, "eheh pensavi che funzionasse...",
-                                Toast.LENGTH_LONG).show();
-                        /* //per ora ho trovato questo
-                        // Acquire a reference to the system Location Manager
-                        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-                       // Define a listener that responds to location updates
-                        LocationListener locationListener = new LocationListener() {
-                            public void onLocationChanged(Location location) {
-                                // Called when a new location is found by the network location provider.
-                                makeUseOfNewLocation(location);
-                            }
-
-                            public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-                            public void onProviderEnabled(String provider) {}
-
-                            public void onProviderDisabled(String provider) {}
-                        };
-
-                        // Register the listener with the Location Manager to receive location updates
-                        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-                        //da aggiungere al gradle le dependencies
-                        */
-                    }
-                }
-        );
 
         mAddCost = (Button) findViewById(R.id.add_cost_add_button);
         mAddCost.setOnClickListener(
@@ -80,10 +70,88 @@ public class AddCostActivity extends AppCompatActivity {
                     public void onClick(View view) {
                         // TODO: 22/06/2016 IMPLEMENTARE CARICAMENTO SU SERVER con richiesta volley
                         Toast.makeText(AddCostActivity.this, "eheh pensavi che funzionasse...",
-                                    Toast.LENGTH_LONG).show();
-                        }
+                                Toast.LENGTH_LONG).show();
                     }
+                }
         );
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mClient = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void onPickButtonClick(View v) {
+        // Construct an intent for the place picker
+        try {
+            PlacePicker.IntentBuilder intentBuilder =
+                    new PlacePicker.IntentBuilder();
+            Intent intent = intentBuilder.build(this);
+            // Start the intent by requesting a result,
+            // identified by a request code.
+            startActivityForResult(intent, REQUEST_PLACE_PICKER);
+
+        } catch (GooglePlayServicesRepairableException e) {
+            // ...
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // ...
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_PLACE_PICKER
+                && resultCode == Activity.RESULT_OK) {
+
+            // The user has selected a place. Extract the name and address.
+            final Place place = PlacePicker.getPlace(this, data);
+
+            final CharSequence name = place.getName();
+            final CharSequence address = place.getAddress();
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mClient.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "AddCost Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://it.unibs.appwow/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(mClient, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "AddCost Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://it.unibs.appwow/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(mClient, viewAction);
+        mClient.disconnect();
     }
 }
