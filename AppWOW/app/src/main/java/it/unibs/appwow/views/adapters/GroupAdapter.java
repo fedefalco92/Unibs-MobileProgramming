5adb6b4416d6462125cf333e3cba0982d219a015
f.falcone <f.falcone@unibs.it>
2016-07-20 23:46:13 +0200

    Aggiunte altre icone, modificata cardview con esse e aggiunte palette

--- AppWOW/app/src/main/java/it/unibs/appwow/views/adapters/GroupAdapter.java
+++ AppWOW/app/src/main/java/it/unibs/appwow/views/adapters/GroupAdapter.java
@@ -2,6 +2,7 @@ package it.unibs.appwow.views.adapters;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
@@ -116,16 +117,22 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
         Bitmap bm = FileUtils.readGroupImage(itemGroup.getId(), mContext);
         if(bm!=null){
             holder.groupImageView.setImageBitmap(bm);
+            // FIXME: 20/07/16 Sistemare PAlette
+            Palette.from(bm).generate(new Palette.PaletteAsyncListener() {
+                public void onGenerated(Palette p) {
+                    Palette.Swatch swatch = p.getVibrantSwatch();
+                    if(swatch != null){
+                        //holder.layout.setBackgroundColor(p.getVibrantColor(mContext.getResources().getColor(R.color.md_grey_700)));
+                        holder.layout.setBackgroundColor(swatch.getRgb());
+                        //holder.groupName.setTextColor(swatch.getTitleTextColor());
+                        //holder.personalStatus.setTextColor(swatch.getBodyTextColor());
+                        //holder.layout.setBackgroundColor(mContext.getResources().getColor(R.color.md_amber_500));
+                    }
+                }
+            });
         } else {
             holder.groupImageView.setImageResource(R.drawable.ic_group_black_48dp);
         }
-
-        // FIXME: 20/07/16 Sistemare PAlette
-        /*Palette.from(bm).generate(new Palette.PaletteAsyncListener() {
-            public void onGenerated(Palette p) {
-                holder.layout.setBackgroundColor(p.getVibrantColor(mContext.getResources().getColor(R.color.md_grey_700)));
-            }
-        });*/
 
 
         if(itemGroup.isHighlighted()){
@@ -133,7 +140,7 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
             holder.groupImageNew.setVisibility(View.VISIBLE);
         } else {
             //holder.groupModified.setText("U");
-            holder.groupImageNew.setVisibility(View.INVISIBLE);
+            holder.groupImageNew.setVisibility(View.GONE);
         }
 
         GroupDAO dao = new GroupDAO();
@@ -145,11 +152,14 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
             Currency curr = Currency.getInstance(Locale.getDefault());
             holder.personalStatus.setText(curr.getSymbol() + " " + Amount.getAmountString(userAmount));
             if(userAmount>0){
-                holder.personalStatus.setTextColor(ContextCompat.getColor(mContext, R.color.green));
+                //holder.personalStatus.setTextColor(ContextCompat.getColor(mContext, R.color.green));
+                holder.groupImageTrending.setImageResource(R.drawable.ic_trending_up_white_24dp);
             } else if(userAmount <0){
-                holder.personalStatus.setTextColor(ContextCompat.getColor(mContext, R.color.red));
+                //holder.personalStatus.setTextColor(ContextCompat.getColor(mContext, R.color.red));
+                holder.groupImageTrending.setImageResource(R.drawable.ic_trending_down_white_24dp);
             } else{
-                holder.personalStatus.setTextColor(ContextCompat.getColor(mContext, R.color.white));
+                //holder.personalStatus.setTextColor(ContextCompat.getColor(mContext, R.color.white));
+                holder.groupImageTrending.setImageResource(R.drawable.ic_trending_flat_white_24dp);
             }
         } else {
             holder.personalStatus.setText("");
@@ -213,6 +223,7 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
         private TextView personalStatus;
         private ImageView groupImageNew;
         private ImageView groupImageFavorite;
+        private ImageView groupImageTrending;
 
         private LinearLayout layout;
 
@@ -224,6 +235,7 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
             groupImageView = (ImageView) itemView.findViewById(R.id.group_tile_imageView);
             groupName = (TextView)itemView.findViewById(R.id.group_tile_groupName);
             personalStatus = (TextView) itemView.findViewById(R.id.group_tile_personalStatus);
+            groupImageTrending = (ImageView) itemView.findViewById(R.id.group_tile_image_trending);
 
             //groupModified = (TextView) itemView.findViewById(R.id.group_tile_modified_indicator);
             groupImageNew = (ImageView) itemView.findViewById(R.id.group_tile_image_new);
@@ -239,7 +251,6 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
             });
         }
 
-        // todo Da sistemare con implementazione piu' efficiente, anche se per ora funziona
         @Override
         public void onClick(View v) {
             Log.d(TAG_LOG, "onClick position: " + getAdapterPosition());
@@ -248,29 +259,6 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
             } else {
                 Log.d(TAG_LOG, "mOnItemClickListener is null");
             }
-
-
-            /*
-            Log.d(TAG_LOG, "click on " + getAdapterPosition());
-            GroupModel group = mItems.get(getAdapterPosition());
-            Log.d(TAG_LOG, "group: " + group.getGroupName());
-            final Intent i = new Intent(mContext, GroupDetailsActivity.class);
-
-            /**
-             * il gruppo che sto passando è highlighted.
-             * Userò questa informazione per aggiornare l'intero gruppo in GroupDetailsActivity
-             *//*
-            i.putExtra(PASSING_GROUP_TAG, group);
-
-            //tolgo l'highlight dal gruppo NEL DB LOCALE, non nell'oggetto passato
-            GroupDAO dao = new GroupDAO();
-            dao.open();
-            dao.unHighlightGroup(group.getId());
-            dao.close();
-
-            //i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            mContext.startActivity(i);*/
         }
 
         @Override
@@ -286,116 +274,3 @@ public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.GroupViewHol
     }
 
 }
-
-
-/*
-public class GroupAdapter extends BaseAdapter {
-    private static final String TAG_LOG = GroupAdapter.class.getSimpleName();
-
-    private List<GroupModel> mItems = new ArrayList<GroupModel>();
-    private final LayoutInflater mInflater;
-    private GroupDAO dao = new GroupDAO();
-    private Context mContext;
-    private LocalUser mLocalUser;
-
-    private class Holder {
-        ImageView groupImageView;
-        TextView groupName;
-        TextView groupModified;
-        TextView personalStatus;
-    }
-
-    public GroupAdapter(Context context){
-        mContext = context;
-        mLocalUser = LocalUser.load(mContext);
-        mInflater = LayoutInflater.from(context);
-        dao.open();
-        mItems = dao.getAllGroups();
-        dao.close();
-        Log.d(TAG_LOG, "Size mItems = "+ mItems.size());
-    }
-
-
-
-
-    @Override
-    public int getCount() {
-        return mItems.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return mItems.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        final GroupModel itemGroup = (GroupModel) getItem(position);
-        return itemGroup.getId();
-    }
-
-    public int getGroupPosition(int idGroup){
-        for(GroupModel g:mItems){
-            if(g.getId()==idGroup)
-                return mItems.indexOf(g);
-        }
-        return -1;
-    }
-
-
-
-    @Override
-    public View getView(final int position, View view, ViewGroup parent) {
-        Holder holder = null;
-        if(view==null) {
-            view = mInflater.inflate(R.layout.tile_group_layout,null);
-            holder = new Holder();
-            holder.groupImageView = (ImageView) view.findViewById(R.id.group_tile_imageView);
-            holder.groupName = (TextView)view.findViewById(R.id.group_tile_groupName);
-            holder.groupModified = (TextView) view.findViewById(R.id.group_tile_modified_indicator);
-            holder.personalStatus = (TextView) view.findViewById(R.id.group_tile_personalStatus);
-            view.setTag(holder);
-        } else {
-            holder = (Holder)view.getTag();
-        }
-        final GroupModel itemGroup = (GroupModel) getItem(position);
-        holder.groupName.setText(itemGroup.getGroupName());
-        if(itemGroup.getPhotoFileName().isEmpty()){
-            holder.groupImageView.setImageResource(getPhotoId(itemGroup.getPhotoFileName()));
-            // TODO: 04/07/2016 scommentare, commentato per debug
-            //holder.groupImageView.setImageBitmap(FileUtils.readBitmap("photo_1467633848847.png", mContext));
-        } else {
-            holder.groupImageView.setImageBitmap(FileUtils.readGroupImage(itemGroup.getId(), mContext));
-        }
-
-        if(itemGroup.isHighlighted()){
-            holder.groupModified.setText("NEW");
-        } else {
-            holder.groupModified.setText("UP TO DATE");
-        }
-
-        GroupDAO dao = new GroupDAO();
-        dao.open();
-        Double userAmount = dao.getAmount(itemGroup.getId(), mLocalUser.getId());
-        dao.close();
-        if (userAmount!=null){
-            holder.personalStatus.setText(Amount.getAmountString(userAmount));
-            if(userAmount>0){
-                holder.personalStatus.setTextColor(ContextCompat.getColor(mContext, R.color.green));
-            } else if(userAmount <0){
-                holder.personalStatus.setTextColor(ContextCompat.getColor(mContext, R.color.red));
-            } else{
-                holder.personalStatus.setTextColor(ContextCompat.getColor(mContext, R.color.white));
-            }
-        } else {
-            holder.personalStatus.setText("");
-        }
-        return view;
-    }
-
-    private int getPhotoId(String photoURI){
-        // TODO: 26/05/2016 GROUP ADAPTER implementare la trasformazione da photo uri (string) a resource ID (int)
-        return R.drawable.ic_menu_send;
-    }
-
-}*/
